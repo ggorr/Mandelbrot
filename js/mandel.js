6af597670d27cfb908493e831c9f0dd5234dd80b
ggorr <ggorr.kp@gmail.com>
2022-11-07 20:58:27 +0900

    Update mandel.js

--- js/mandel.js
+++ js/mandel.js
@@ -1,22 +1,21 @@
 let unit, minX, maxX, minY, maxY;
-let offset = 10;
-function setView() {
+const setView = () => {
     unit = parseFloat(document.getElementById('unit').value);
     minX = parseFloat(document.getElementById('min-x').value);
     maxX = parseFloat(document.getElementById('max-x').value);
     minY = parseFloat(document.getElementById('min-y').value);
     maxY = parseFloat(document.getElementById('max-y').value);
     document.getElementById('view').innerHTML = `(${Math.round((maxX - minX) * unit)}, ${Math.round((maxY - minY) * unit)})`;
-}
-function trunc(x) {
+};
+const trunc = (x) => {
     return Math.round(x * 10000000000) / 10000000000;
-}
-function setXy(x, y) {
-    document.getElementById('xy').innerHTML = `(${trunc(minX + (x - offset) / unit)}, ${trunc(maxY - (y - offset) / unit)})`;
-}
-function centerTo(canvasX, canvasY) {
-    let x = minX + (canvasX - offset) / unit;
-    let y = maxY - (canvasY - offset) / unit;
+};
+const setXy = (x, y) => {
+    document.getElementById('xy').innerHTML = `(${trunc(minX + x / unit)}, ${trunc(maxY - y / unit)})`;
+};
+const centerTo = (canvasX, canvasY) => {
+    let x = minX + canvasX / unit;
+    let y = maxY - canvasY / unit;
     let sx = x - (minX + maxX) / 2;
     minX += sx;
     maxX += sx;
@@ -28,8 +27,8 @@ function centerTo(canvasX, canvasY) {
     document.getElementById('min-y').value = `${trunc(minY)}`;
     document.getElementById('max-y').value = `${trunc(maxY)}`;
     setView(); // test
-}
-function expand(factor) {
+};
+const expand = (factor) => {
     unit *= factor;
     let t = minX;
     minX = ((factor + 1) * t + (factor - 1) * maxX) / (2 * factor);
@@ -43,10 +42,10 @@ function expand(factor) {
     document.getElementById('min-y').value = `${trunc(minY)}`;
     document.getElementById('max-y').value = `${trunc(maxY)}`;
     setView(); // test
-}
+};
 let worker = null;
 let interval;
-function display() {
+const display = () => {
     if (worker != null)
         return;
     worker = new Worker("./js/worker.js", { type: 'module' });
@@ -62,7 +61,7 @@ function display() {
     worker.onmessage = (ev) => {
         for (let i = 0; i < ev.data.length; i++)
             image.data[pos++] = ev.data[i];
-        context.putImageData(image, offset, offset);
+        context.putImageData(image, 0, 0);
         if (pos == 4 * width * height)
             stopDisplay();
     };
@@ -72,12 +71,24 @@ function display() {
         timeCount++;
         document.getElementById("time").innerHTML = (timeCount / 10).toFixed(1);
     }, 100);
-}
-function stopDisplay() {
+};
+const stopDisplay = () => {
     if (worker == null)
         return;
     clearInterval(interval);
     worker.terminate();
     worker = null;
-}
-export { setView, setXy, centerTo, expand, display, stopDisplay };
+};
+const downloadPng = () => {
+    // download
+    let canvas = document.getElementById('canvas');
+    canvas.toBlob((blob) => {
+        if (blob != null) {
+            let link = document.createElement("a");
+            link.download = document.getElementById('filename').value;
+            link.href = window.URL.createObjectURL(blob);
+            link.click();
+        }
+    }, 'image/png');
+};
+export { setView, setXy, centerTo, expand, display, stopDisplay, downloadPng };
